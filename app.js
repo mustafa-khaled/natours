cdd001d59aa6e7bef95f163403a84c68f79db306
Mustafa Khaled <mostafakhaled0787314@gmail.com>
2025-05-20 10:38:02 +0300

    create and get a single tour

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`),
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
 );
+
+app.use(express.json());
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
@@ -17,6 +19,47 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const tour = tours.find((t) => t.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'Fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = {
+    id: newId,
+    ...req.body,
+  };
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    },
+  );
 });
 
 app.listen(PORT, () => {
