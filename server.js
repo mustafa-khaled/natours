7aac80fc357c83a411396ed485a3729400eb093a
Mustafa Khaled <mostafakhaled0787314@gmail.com>
2025-05-24 11:01:09 +0300

    connect to DB

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE_URL.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful!'));
 
 const app = require('./app');
 
