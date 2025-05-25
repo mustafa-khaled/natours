556a152d3a78854d02ae4acf8ebee531595172b1
Mustafa Khaled <mostafakhaled0787314@gmail.com>
2025-05-25 22:37:04 +0300

    modeling tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,16 +4,53 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
+    trim: true,
     unique: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have an cover image'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  discount: Number,
+  images: [String],
+  startDates: [Date],
 });
 
 module.exports = mongoose.model('Tour', tourSchema);
