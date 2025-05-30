2656e693c8b3927a62cc01304d7b3710a973f5b9
Mustafa Khaled <mostafakhaled0787314@gmail.com>
2025-05-30 18:35:05 +0300

    implement sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,13 +2,25 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    // 1A): Filtering
     const queryObj = { ...req.query };
-
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
-
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    // 1B): Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2): Sort
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
     const tours = await query;
 
     res.status(200).json({
