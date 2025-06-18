6255db18c884badf80375c341740515b3ce47b69
Mustafa Khaled <mostafakhaled0787314@gmail.com>
2025-06-18 05:20:46 +0300

    start working on User schema

---
+++ models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please enter a valid email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+  },
+  photo: String,
+});
+
+module.exports = mongoose.model('User', userSchema);
