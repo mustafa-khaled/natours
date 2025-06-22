7ec00d14dc4978985729344aee76743048635105
Mustafa Khaled <mostafakhaled0787314@gmail.com>
2025-06-22 21:28:31 +0300

    generate token when signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,33 @@
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
 const { catchAsync } = require('../utils/catchAsync');
 
 const signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const { name, email, password, passwordConfirm } = req.body;
+
+  const newUser = await User.create({
+    name,
+    email,
+    password,
+    passwordConfirm,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(200).json({
     message: 'success',
+    token,
     data: {
       user: newUser,
     },
   });
 });
 
+const login = catchAsync(async (req, res, next) => {});
+
 module.exports = {
   signup,
+  login,
 };
