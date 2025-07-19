ca9ae8f31a338062aaf7ac850ee78f5610b963fe
Mustafa Khaled <mostafakhaled0787314@gmail.com>
2025-07-19 13:05:24 +0300

    add security middlewares

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,22 +13,55 @@ const userRouter = require('./routes/userRouter');
 
 const app = express();
 
-// GLOBAL middlewares
+// 1): {{ GLOBAL middlewares }}
+
+// Set Security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit the request number from same IP.
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'To many request from this IP, please try again in an hour!',
 });
-
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(
+  express.json({
+    limit: '10kb',
+  }),
+);
+
+// Data sanitization against noSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// 2): {{ ROUTES }}
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
