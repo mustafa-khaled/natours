4823ef57768d0455d370aad00249bbc74e941db0
Mustafa Khaled <mostafakhaled0787314@gmail.com>
2025-07-26 15:00:01 +0300

    build nested routes tours => reviews

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -5,7 +5,9 @@ const {
 } = require('../controllers/reviewController');
 const { protect, restrictTo } = require('../controllers/authController');
 
-const router = express.Router();
+// MERGE PARAMS => to allow child routers to access route parameters defined in their parent routers.
+// In this case => Access :/tourId from the tours router inside this child router
+const router = express.Router({ mergeParams: true });
 
 router
   .route('/')
