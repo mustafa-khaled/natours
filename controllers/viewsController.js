5d93aaf214d1ec11a4622ed492d7116701dfe086
Mustafa Khaled <mostafakhaled0787314@gmail.com>
2025-08-09 10:45:43 +0300

    build: overview page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,18 @@
-const getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const { catchAsync } = require('../utils/catchAsync');
+
+const getOverview = catchAsync(async (req, res, next) => {
+  // 1): Get tour data from collection
+  const tours = await Tour.find();
+
+  // 2): Build template
+
+  // 3): Render that template using tour data from 1
   res.status(200).render('overview', {
     title: 'All tours',
+    tours,
   });
-};
+});
 
 const getTour = (req, res) => {
   res.status(200).render('tour', {
